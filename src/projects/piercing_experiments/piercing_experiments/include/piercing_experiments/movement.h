e632e92e2a15e10a27f29e5aefa0cf6e5ea4252e
Calli2 <abhinavgandhi09@gmail.com>
2020-04-05 16:45:22 -0400

    Removed generic moveit config

--- src/projects/piercing_experiments/piercing_experiments/include/piercing_experiments/movement.h
+++ src/projects/piercing_experiments/piercing_experiments/include/piercing_experiments/movement.h
@@ -1,17 +1,18 @@
-#pragma once
+// This is a work in progress
+// #pragma once
 
-class Movement{
+// class Movement{
 
-public:
-Movement(ros::NodeHandle& _n);
-bool prepose_callback(piercing_experiments_msgs::PrePose::Request& req, piercing_experiments_msgs::PrePose::Response& res);
-bool cartesian_path_callback(piercing_experiments_msgs::CartesianPath::Request& req, piercing_experiments_msgs::CartesianPath::Response& res);
+// public:
+// Movement(ros::NodeHandle& _n);
+// bool prepose_callback(piercing_experiments_msgs::PrePose::Request& req, piercing_experiments_msgs::PrePose::Response& res);
+// bool cartesian_path_callback(piercing_experiments_msgs::CartesianPath::Request& req, piercing_experiments_msgs::CartesianPath::Response& res);
 
 
-private:
-ros::NodeHandle n;
-moveit::planning_interface::MoveGroupInterface move_group;
-ros::ServiceServer prepose_server;
-ros::ServiceServer cartesian_server;
+// private:
+// ros::NodeHandle n;
+// moveit::planning_interface::MoveGroupInterface move_group;
+// ros::ServiceServer prepose_server;
+// ros::ServiceServer cartesian_server;
 
-};
+// };
