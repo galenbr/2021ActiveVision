d88c09c433e7d04061ddd367d8b0a61e959bccca
Calli2 <abhinavgandhi09@gmail.com>
2020-04-13 15:56:55 -0400

    Pcl conversions and downsampling

--- ros_ws/src/projects/lock_key/lock_key/src/find_key.cpp
+++ ros_ws/src/projects/lock_key/lock_key/src/find_key.cpp
@@ -1,10 +1,43 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <lock_key/findKey.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/filters/voxel_grid.h>
+#include <iostream>
 
 bool key_service_callback(lock_key::findKey::Request &req, lock_key::findKey::Response &res){
+    ROS_INFO("FINDING KEY");
+    // Convert sensor_msgs to pcl pointcloud2
+    // TO DO: Put in a function
+    pcl::PCLPointCloud2::Ptr pcl_pc2 (new pcl::PCLPointCloud2 ());
+    
+    pcl_conversions::toPCL(req.pc2, *pcl_pc2);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromPCLPointCloud2(*pcl_pc2, *cloud);
 
-    //Do pointcloud stuff here to find key
+    //Downsampling the pointcloud
+    // TO DO: Put in a function
+    pcl::PCLPointCloud2::Ptr cloud_filtered(new pcl::PCLPointCloud2());
+
+    // std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height 
+    //    << " data points (" << pcl::getFieldsList (*cloud) << ").";
+
+    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+    sor.setInputCloud (pcl_pc2);
+    sor.setLeafSize(0.01f, 0.01f, 0.01f);
+    sor.filter(*cloud_filtered);
+
+    // std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height 
+    //    << " data points (" << pcl::getFieldsList (*cloud_filtered) << ").";
+
+    // Lets Try Segmentation
+    // TO DO: Put in a function
+
+    
 
 
 
@@ -16,7 +49,7 @@ int main(int argc, char **argv){
 
     ros::init(argc, argv, "findKey");
     ros::NodeHandle n;
-
+    
     ros::ServiceServer key = n.advertiseService("findKeyServer", key_service_callback);
 
     ros::spin();
