a867564a2594eb1e8fbbe52488bdd7d4f7bfe58e
Calli2 <abhinavgandhi09@gmail.com>
2020-04-14 10:58:53 -0400

    Added centroid vis

--- ros_ws/src/projects/lock_key/lock_key/src/controller.cpp
+++ ros_ws/src/projects/lock_key/lock_key/src/controller.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
+// Services
 #include <lock_key/imgCapture.h>
 #include <lock_key/findKey.h>
+// Publish Marker
+#include <visualization_msgs/Marker.h>
 
 void wait_for_service(int32_t timeout){
     ros::service::waitForService("move_to_pose",timeout);
@@ -29,11 +32,37 @@ int main(int argc, char **argv){
     findKeyClient.call(keyImg);
     ROS_INFO("Key found");
 
-    ros::Publisher pub = n.advertise<sensor_msgs::PointCloud2>("debugPC2", 5);
+    ros::Publisher cloud_pub = n.advertise<sensor_msgs::PointCloud2>("debugPC2", 1);
+    ros::Publisher centroid_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+
+    visualization_msgs::Marker marker;
+
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.scale.x = .005;
+    marker.scale.y = .005;
+    marker.scale.z = .005;
+    marker.color.a = 1;
+    marker.color.r = 1;
+    marker.color.g = 1;
+    marker.color.b = 1;
+    marker.pose.orientation.w = 1;
+    marker.header.frame_id="/panda_camera_optical_link";
+    marker.header.stamp = ros::Time();
+    marker.id = 0;
+    marker.pose.position.x = keyImg.response.p.x;
+    marker.pose.position.y = keyImg.response.p.y;
+    marker.pose.position.z = keyImg.response.p.z;
+
+    ROS_INFO_STREAM(marker.pose);
+
+
     while (true)
     {
-        pub.publish(keyImg.response.pc2);
-        ROS_INFO("KEY Published");
+        cloud_pub.publish(keyImg.response.pc2);
+        centroid_pub.publish(marker);
+        //ROS_INFO("KEY Published");
     }
     
     ros::spin();
