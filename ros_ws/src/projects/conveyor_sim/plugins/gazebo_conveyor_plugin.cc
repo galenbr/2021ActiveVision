14001252555c24aad23b81376768bdcca1bc783d
Fadi Alladkani <fadialladkani@gmail.com>
2020-05-15 16:05:26 -0700

    Conveyor in any position now possible

--- ros_ws/src/projects/conveyor_sim/plugins/gazebo_conveyor_plugin.cc
+++ ros_ws/src/projects/conveyor_sim/plugins/gazebo_conveyor_plugin.cc
@@ -18,16 +18,19 @@ namespace gazebo {
 	gzerr << "No conveyor speed specified\n";
       if(!_sdf->HasElement("length"))
 	gzerr << "No conveyor length specified\n";
+      if(!_sdf->HasElement("root"))
+	gzerr << "No conveyor root specified\n";
 
       speed = _sdf->GetElement("speed")->Get<double>();
       length = _sdf->GetElement("length")->Get<double>();
+      root = _sdf->GetElement("root")->Get<double>();
     }
 
     void OnUpdate() {
       this->model->SetLinearVel(ignition::math::Vector3d(0, speed, 0));
       curPose = this->model->GetLink()->WorldPose();
-      if(curPose.Pos().Y() >= length) {
-	curPose.Pos().Y() = 0.0; // Hardcoded start position, should change to initial position
+      if(curPose.Pos().Y() - root >= length) {
+	curPose.Pos().Y() = root; // Hardcoded start position, should change to initial position
 	this->model->SetLinkWorldPose(curPose, "canonical");
       }
     }
@@ -35,6 +38,7 @@ namespace gazebo {
     // Parameters
     double speed;
     double length;
+    double root;
 
     // Other
     ignition::math::Pose3d curPose;
--- ros_ws/src/projects/conveyor_sim/src/conveyor_sim.cpp
+++ ros_ws/src/projects/conveyor_sim/src/conveyor_sim.cpp
@@ -6,9 +6,10 @@
 #include <gazebo_msgs/ModelStates.h>
 
 double conveyor_width;
-double conveyor_segment_length{0.05}; // Hardcoded
+double conveyor_segment_length;
 double conveyor_length;
 double conveyor_speed;
+double y, x, z;
 std::string conveyor_urdf;
 
 int main(int argc, char** argv) {
@@ -18,16 +19,16 @@ int main(int argc, char** argv) {
   if(!nh.getParam("/conveyor/width", conveyor_width) ||
      !nh.getParam("/conveyor/length", conveyor_length) ||
      !nh.getParam("/conveyor/speed", conveyor_speed) ||
-     !nh.getParam("/conveyor/description",  conveyor_urdf)) {
+     !nh.getParam("/conveyor/description",  conveyor_urdf) ||
+     !nh.getParam("/conveyor/seg_length", conveyor_segment_length) ||
+     !nh.getParam("/conveyor/y", y) ||
+     !nh.getParam("/conveyor/x", x) ||
+     !nh.getParam("/conveyor/z", z)) {
     ROS_ERROR("Could not load conveyor parameters");
     return 1;
   }
 
-  ros::Duration d{0.1};
-  while(!ros::service::exists("/gazebo/spawn_urdf_model", true)) {
-    ROS_WARN("Waiting for spawn_urdf_model service");
-    d.sleep();
-  }
+  ros::service::waitForService("/gazebo/spawn_urdf_model");
 
   ros::ServiceClient spawnModelClient = nh.serviceClient<gazebo_msgs::SpawnModel>("/gazebo/spawn_urdf_model");
 
@@ -35,10 +36,13 @@ int main(int argc, char** argv) {
   gazebo_msgs::SpawnModel modelSpawn;
   modelSpawn.request.model_xml = conveyor_urdf;
   modelSpawn.request.initial_pose.orientation.w = 1;
+  modelSpawn.request.initial_pose.position.x = x;
+  modelSpawn.request.initial_pose.position.z = z;
+  modelSpawn.request.initial_pose.position.y = y;
   for(int i = 0; i < (int)(conveyor_length/conveyor_segment_length); ++i) {
     modelSpawn.request.model_name = "segment_" + std::to_string(i);
-    modelSpawn.request.initial_pose.position.y += conveyor_segment_length;
     spawnModelClient.call(modelSpawn);
+    modelSpawn.request.initial_pose.position.y += conveyor_segment_length;
   }
 
   return 0;
