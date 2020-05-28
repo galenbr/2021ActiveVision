16d6eab28be9e78c13a925a1b8958a1e65305203
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2020-05-28 17:49:59 -0400

    reverting to force controllers

--- ros_ws/src/projects/lock_key/lock_key/src/controller.cpp
+++ ros_ws/src/projects/lock_key/lock_key/src/controller.cpp
@@ -39,7 +39,7 @@ int main(int argc, char **argv){
     ros::ServiceClient movePoseClient2 = n.serviceClient<moveit_planner::MovePose>("move_to_pose");
     ros::ServiceClient moveCartClient = n.serviceClient<moveit_planner::MoveCart>("cartesian_move");
     ros::ServiceClient gripperClient = n.serviceClient<franka_gripper_gazebo::GripMsg>("gazebo_franka_grip");
-    ros::ServiceClient gripperPosClient = n.serviceClient<franka_pos_grasping_gazebo::GripPos>("gripPosServer");
+    // ros::ServiceClient gripperPosClient = n.serviceClient<franka_pos_grasping_gazebo::GripPos>("gripPosServer");
 
     // Client Objects
     lock_key::imgCapture reqImg;
@@ -96,70 +96,76 @@ int main(int argc, char **argv){
         ROS_INFO("KEY Published");
         count += 1;
      }
-     ROS_INFO("Moving finger");
-     grasp.request.finger_pos = 0.025;
-     gripperPosClient.call(grasp);
-    // //Manipulating position so as to create pre-grasp pose
-    // keypose.point.x -= 0.06;
-    // keypose.point.z -= 0.006;
+    //     // Opening Gripper
+    //  grasp.request.finger_pos = 0.04;
+    //  gripperPosClient.call(grasp);
+    //Manipulating position so as to create pre-grasp pose
+    keypose.point.x -= 0.06;
+    keypose.point.z -= 0.006;
     
-    // //Setting pre-grasp orientation
-    // pose.request.val.position = keypose.point;
-    // pose.request.val.orientation.x = 0.0;//0.3826834;// 0.7071068; //-0.4082483;
-    // pose.request.val.orientation.y = 0.7071068; //0.8164966;
-    // pose.request.val.orientation.z = 0.0; //-0.4082483;
-    // pose.request.val.orientation.w = 0.7071068;
+    //Setting pre-grasp orientation
+    pose.request.val.position = keypose.point;
+    pose.request.val.orientation.x = 0.0;//0.3826834;// 0.7071068; //-0.4082483;
+    pose.request.val.orientation.y = 0.7071068; //0.8164966;
+    pose.request.val.orientation.z = 0.0; //-0.4082483;
+    pose.request.val.orientation.w = 0.7071068;
 
-    // // Opening Gripper and moving to pre-grasp pose
-    // pose.request.execute = true;
-    // grip.request.force = 50.0;
-    // gripperClient.call(grip);
-    // movePoseClient.call(pose);
+    // Opening Gripper and moving to pre-grasp pose
+    pose.request.execute = true;
+    grip.request.force = 50.0;
+    gripperClient.call(grip);
+    movePoseClient.call(pose);
 
-    // // Move to grasp
-    // geometry_msgs::Pose p;
-    // p.position = keypose.point;
-    // p.orientation = pose.request.val.orientation;
-    // p.position.x += 0.04;   
-    // cart.request.val.push_back(p);
-    // cart.request.execute = true;
-    // moveCartClient.call(cart);
-    // ros::Duration(1).sleep();
-    // // Grasp
-    // ROS_INFO("Gripping");
-    // grip.request.force = -0.10;
-    // gripperClient.call(grip);
-    // ROS_INFO("Gripped");
-    // ros::Duration(1).sleep();
-    // grip.request.force = -20.0;
-    // gripperClient.call(grip);
-    // ros::Duration(0.1).sleep();
-    // grip.request.force = -20.0;
-    // gripperClient.call(grip);
-    // ros::Duration(0.1).sleep();
-    // // grip.request.force = -50.0;
-    // // gripperClient.call(grip);
-    // // ros::Duration(0.2).sleep();
-    // // Move away
-    // cart2.request.val.push_back(p);
-    // p.position.x -= 0.04;
-    // cart2.request.val.push_back(p);
-    // p.position.z += 0.05;
-    // cart2.request.val.push_back(p);
-    // cart2.request.execute = true;
-    // moveCartClient.call(cart2);
-    // ros::Duration(1).sleep();
-    // //Move to Lock
-    // ROS_INFO("Moving to Lock");
-    // pose2.request.val.position.x = 0.65;
-    // pose2.request.val.position.y = 0.0;
-    // pose2.request.val.position.z = 0.45;
-    // pose2.request.val.orientation.w = 0.00653015;
-    // pose2.request.val.orientation.x = 0.915333;
-    // pose2.request.val.orientation.y = 0.402644;
-    // pose2.request.val.orientation.z = 0.000463674;
-    // pose2.request.execute = true;
-    // movePoseClient2.call(pose2);
+    // Move to grasp
+    geometry_msgs::Pose p;
+    p.position = keypose.point;
+    p.orientation = pose.request.val.orientation;
+    p.position.x += 0.04;   
+    cart.request.val.push_back(p);
+    cart.request.execute = true;
+    moveCartClient.call(cart);
+    ros::Duration(1).sleep();
+    // Grasp
+    ROS_INFO("Gripping");
+    // grasp.request.finger_pos = 0.0;
+    // gripperPosClient.call(grasp);
+    // ros::Duration(3).sleep();
+
+
+    // For the force controller
+    grip.request.force = -0.10;
+    gripperClient.call(grip);
+    ROS_INFO("Gripped");
+    ros::Duration(1).sleep();
+    grip.request.force = -20.0;
+    gripperClient.call(grip);
+    ros::Duration(0.1).sleep();
+    grip.request.force = -20.0;
+    gripperClient.call(grip);
+    ros::Duration(0.1).sleep();
+    grip.request.force = -50.0;
+    gripperClient.call(grip);
+    ros::Duration(0.2).sleep();
+    // Move away
+    cart2.request.val.push_back(p);
+    p.position.x -= 0.04;
+    cart2.request.val.push_back(p);
+    p.position.z += 0.05;
+    cart2.request.val.push_back(p);
+    cart2.request.execute = true;
+    moveCartClient.call(cart2);
+    ros::Duration(1).sleep();
+    //Move to Lock
+    ROS_INFO("Moving to Lock");
+    pose2.request.val.position.x = 0.65;
+    pose2.request.val.position.y = 0.0;
+    pose2.request.val.position.z = 0.45;
+    pose2.request.val.orientation.w = 0.00653015;
+    pose2.request.val.orientation.x = 0.915333;
+    pose2.request.val.orientation.y = 0.402644;
+    pose2.request.val.orientation.z = 0.000463674;
+    pose2.request.execute = true;
+    movePoseClient2.call(pose2);
     ros::spin();
 
     return 0;
