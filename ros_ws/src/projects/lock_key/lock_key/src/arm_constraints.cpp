a6c95efa65f7ae20aa0d62d1cf2281e11fc28291
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2020-06-03 12:40:10 -0400

    constrained join 2

--- ros_ws/src/projects/lock_key/lock_key/src/arm_constraints.cpp
+++ ros_ws/src/projects/lock_key/lock_key/src/arm_constraints.cpp
@@ -1,25 +1,21 @@
 #include <ros/ros.h>
-#include <moveit_msgs/JointConstraint.h>
-#include <moveit_msgs/Constraints.h>
+#include "moveit_planner/SetConstraints.h"
 
 int main(int argc, char **argv){
 
 ros::init(argc, argv, "arm_constraints");
 ros::NodeHandle n;
 
+ros::ServiceClient constraintClient = n.serviceClient<moveit_planner::SetConstraints>("set_constraints");
+moveit_planner::SetConstraints cons ;
 moveit_msgs::JointConstraint jc;
-    jc.position = -0.765;
-    jc.tolerance_above = 3.14159;
+    jc.position = 0.0;
+    jc.tolerance_above = 1.4;//3.14159;
     jc.tolerance_below = 0.4;
     jc.joint_name = "panda_joint2";
     jc.weight = 1;
 
-    moveit_msgs::Constraints cons;
-    cons.joint_constraints.push_back(jc);
-    // moveit::planning_interface::MoveGroupInterface moveGroup;
-    // moveGroup.setPathConstraints(cons);
-
-
+    cons.request.constraints.joint_constraints.push_back(jc);
 
     return 0;
 }
