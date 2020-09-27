0099418383649910900c9887ccb9a4928c09aa95
galenbr <gibrown@wpi.edu>
2020-09-27 13:34:58 -0400

    Started supervisor node pseudocode_2

---
+++ ros_ws/src/projects/active_vision/src/supervisorNode.cpp
@@ -0,0 +1,35 @@
+#include <ros/ros.h>
+#include <iostream>
+//#include <active_vision/testingModel.h>
+
+#define TIMEOUT 10
+
+int main (int argc, char** argv){
+	ros::init (argc, argv, "Supervisor_Node");
+ 	ros::NodeHandle nh;
+
+ 	int stepsTaken = 0;
+
+ 	while(stepsTaken < TIMEOUT){
+
+ 		//Get new data
+
+ 		//Fuse with old data
+
+ 		//Grasp synthesis
+
+ 		//Grasp checking
+
+ 			//Exit if grasp good/timeout exceeded
+
+ 		//Build model
+
+ 		//Get instruction from optimization policy
+
+ 		//Move camera
+
+ 		std::cout << "Spinning... " << stepsTaken << std::endl;
+ 		stepsTaken++;
+ 		sleep(1);
+ 	}
+ }
