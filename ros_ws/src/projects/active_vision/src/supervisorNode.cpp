294ca4576b472bf5413025ae2f277179c3752b98
galenbr <gibrown@wpi.edu>
2020-09-28 11:51:26 -0400

    Improved supervisor control

--- ros_ws/src/projects/active_vision/src/supervisorNode.cpp
+++ ros_ws/src/projects/active_vision/src/supervisorNode.cpp
@@ -4,18 +4,24 @@
 
 #define TIMEOUT 10
 
+void modifyTargetPosition(float *pos){
+	pos[0]+=0.1;
+	pos[1]+=0.1;
+}
+
 int main (int argc, char** argv){
 	ros::init (argc, argv, "Supervisor_Node");
  	ros::NodeHandle nh;
 
- 	environment supervisor = environment(&nh);
+ 	environment kinectControl = environment(&nh);
 
  	int stepsTaken = 0;
+ 	float target_position[6] = {0.25, 0.0, 1.75, 0.0, 0.55, 0.0};
 
  	while(stepsTaken < TIMEOUT){
 
  		//Get new data
- 		testKinectRead(supervisor);
+ 		testKinectRead(kinectControl);
 
  		//Fuse with old data
 
@@ -28,9 +34,11 @@ int main (int argc, char** argv){
  		//Build model
 
  		//Get instruction from optimization policy
+ 		modifyTargetPosition(target_position);
 
  		//Move camera
- 		testKinectMovement(supervisor);
+ 		kinectControl.moveKinect(target_position);
+ 		
 
  		std::cout << "Spinning... " << stepsTaken << std::endl;
  		stepsTaken++;
