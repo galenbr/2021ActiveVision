873b1f686c08e56dd142473f6bbe25dc31f10ac7
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2020-10-23 13:26:53 -0400

    Added owi control node

---
+++ ros_ws/src/projects/soft_manipulation/owi_robot/src/main.cpp
@@ -0,0 +1,19 @@
+#include <ros/ros.h>
+
+#include "owi_robot/generatecmd.h"
+#include "owi_robot/writecmd.h"
+// #include "marker_track/"
+
+int main(int argc, char **argv){
+    
+    ros::init(argc, argv, "owi_control");
+    ros::NodeHandle n;
+
+
+    
+
+
+
+
+    return 0;
+}
