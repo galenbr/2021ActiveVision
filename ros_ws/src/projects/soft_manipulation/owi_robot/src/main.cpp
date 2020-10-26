c358bc7c023dcb2d2e4356aafa80221d62056697
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2020-10-25 20:19:33 -0400

    Removed global vars goalPose & err

--- ros_ws/src/projects/soft_manipulation/owi_robot/src/main.cpp
+++ ros_ws/src/projects/soft_manipulation/owi_robot/src/main.cpp
@@ -5,20 +5,25 @@
 #include "owi_robot/writecmd.h"
 
 geometry_msgs::Point curPose_;
-geometry_msgs::Point goalPose_;
-geometry_msgs::Point err_;
 
-void get_error(){
+geometry_msgs::Point get_error(geometry_msgs::Point goalPose){
 
-    err_.x = goalPose_.x - curPose_.x;
-    err_.y = goalPose_.y - curPose_.y;
-    err_.z = goalPose_.z - curPose_.z;
+    geometry_msgs::Point err;
+    err.x = goalPose.x - curPose_.x;
+    err.y = goalPose.y - curPose_.y;
+    err.z = goalPose.z - curPose_.z;
+
+    return err;
 }
 
-void get_goal_pose(){
-    goalPose_.x = 0.0;  //Read from YAML
-    goalPose_.y = 0.0;
-    goalPose_.z = 0.0;
+geometry_msgs::Point get_goal_pose(){
+
+    geometry_msgs::Point goalPose;
+    goalPose.x = 0.0;  //Read from YAML
+    goalPose.y = 0.0;
+    goalPose.z = 0.0;
+
+    return goalPose;
 }
 
 void get_cur_pose(const geometry_msgs::Point &msg){
@@ -35,13 +40,11 @@ int main(int argc, char **argv){
     ros::NodeHandle n;
 
     ros::Subscriber sub = n.subscribe("object_position",1,get_cur_pose);
-    get_goal_pose();
+    
+    geometry_msgs::Point goal = get_goal_pose();
+    geometry_msgs::Point err = get_error(goal);
 
-    geometry_msgs::Point err;
-
-    get_error();
-
-    if(err_.x>1e2){
+    if(err.x>1e2){
         // move robot
     }
 
