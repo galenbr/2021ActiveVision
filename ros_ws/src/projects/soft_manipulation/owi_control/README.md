cad558bfc1fd8ccf2ce5bb4d0dfda41ee540d893
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2020-11-09 22:27:55 -0500

    owi visual servo

---
+++ ros_ws/src/projects/soft_manipulation/owi_control/README.md
@@ -0,0 +1,1 @@
+This package is used to control the OWI robot via a visual servoing framework.
---
+++ ros_ws/src/projects/soft_manipulation/owi_control/src/owi_controller.cpp
@@ -0,0 +1,100 @@
+#include <ros/ros.h>
+#include <eigen3/Eigen/Dense>
+#include <math.h>
+
+#include "std_msgs/Float64MultiArray.h"
+#include "std_msgs/Float64.h"
+#include "geometry_msgs/Vector3.h"
+#include "geometry_msgs/Point.h"
+
+#define PI 3.14159265/180
+
+geometry_msgs::Point curPose_;
+
+geometry_msgs::Point get_goal_pose(){
+
+    geometry_msgs::Point goalPose;
+    goalPose.x = 0.23;  //Read from YAML
+    goalPose.y = -0.14;
+    goalPose.z = -0.17;
+
+    return goalPose;
+}
+
+void get_cur_pose(const geometry_msgs::Point &msg){
+
+    curPose_.x = msg.x;
+    curPose_.y = msg.y;
+    curPose_.z = msg.z;
+}
+
+int main(int argc, char **argv){
+    
+    ros::init(argc, argv, "owi_controller");
+    ros::NodeHandle n;
+
+    
+    ros::Subscriber sub = n.subscribe("object_position",1,get_cur_pose);
+    ros::Publisher pub = n.advertise<std_msgs::Float64MultiArray>("pwm",1);
+    geometry_msgs::Point goal = get_goal_pose();
+    
+    Eigen::Vector3d f(curPose_.x, curPose_.y, curPose_.z);
+    Eigen::Vector3d f_d(goal.x, goal.y, goal.z);
+    Eigen::Vector3d err;
+    Eigen::Vector3d t_dot;
+    Eigen::Vector3d j_dot;
+
+    err = f_d - f;
+    float u = 0;
+    float v = 0;
+    float lam = 0.035; // in m
+    float z = 0.1;
+    int kp = 1;
+    float l1 = 0.09;    // in m
+    float l2 = 0.065;   // in m
+    float th1 = 0;
+    float th2 = 0;
+
+    float jr1 = - l1*sin(th1*PI) - l2*sin((th1+th2)*PI);
+    float jr2 = - l2*sin((th1 + th2)*PI);
+    float jr3 = l1*cos(th1*PI) + l2*cos((th1 + th2)*PI);
+    float jr4 = l2*cos((th1 + th2)*PI);
+
+    Eigen::MatrixXd Jv(2,2); // Image Jacobian
+    Jv << lam/z,0,
+         0,lam/z;
+
+    Eigen::MatrixXd Kp(2,2); //Gains matrix
+    Kp << kp, 0,
+          0, kp;
+
+    Eigen::MatrixXd Jv_inv(2,2);
+    if(Jv.determinant() != 0)
+        Jv_inv = Jv.inverse();
+
+    Eigen::MatrixXd Jr(2,2); // Robot Jacobian 
+    Jr << jr1, jr2,
+          jr3, jr4;
+
+    Eigen::MatrixXd Jr_inv(2,2);
+    if(Jr.determinant() != 0)
+        Jr_inv = Jr.inverse();
+
+    t_dot = (Kp * Jv_inv) * err;
+
+    j_dot = Jr_inv * t_dot;
+
+    std_msgs::Float64MultiArray vel_msg;    //Velocity Message for Pub
+    vel_msg.data.resize(2);
+    vel_msg.data[0] = j_dot(1);
+    vel_msg.data[1] = j_dot(2);
+
+    pub.publish(vel_msg);
+
+
+
+
+
+    ros::spin();
+    return 0;
+}
