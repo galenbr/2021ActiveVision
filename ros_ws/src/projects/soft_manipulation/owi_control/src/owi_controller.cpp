0e446e7d46a2c153bef01d7467b54032326c647c
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2020-11-11 22:41:38 -0500

    visual servoing for single link

--- ros_ws/src/projects/soft_manipulation/owi_control/src/owi_controller.cpp
+++ ros_ws/src/projects/soft_manipulation/owi_control/src/owi_controller.cpp
@@ -9,14 +9,19 @@
 
 #define PI 3.14159265/180
 
+ros::NodeHandle* nh;
+ros::Publisher pub;
+ros::Publisher err_pub;
+
 geometry_msgs::Point curPose_;
 
 geometry_msgs::Point get_goal_pose(){
 
     geometry_msgs::Point goalPose;
-    goalPose.x = 0.23;  //Read from YAML
-    goalPose.y = -0.14;
-    goalPose.z = -0.17;
+    
+    goalPose.x = -0.096145;  //Read from YAML
+    goalPose.y = -0.049568;
+    goalPose.z = -0.007454;
 
     return goalPose;
 }
@@ -25,107 +30,145 @@ void get_cur_pose(const geometry_msgs::Point &msg){
 
     curPose_.x = msg.x;
     curPose_.y = msg.y;
-    curPose_.z = msg.z;
-}
-
-int main(int argc, char **argv){
-    
-    ros::init(argc, argv, "owi_controller");
-    ros::NodeHandle n;
-
-    
-    ros::Subscriber sub = n.subscribe("object_position",1,get_cur_pose);
-    ros::Publisher pub = n.advertise<std_msgs::Float64MultiArray>("pwm",1);
-    ros::Publisher err_pub = n.advertise<std_msgs::Float64MultiArray>("error",1);
+    curPose_.z = msg.z;   
 
     geometry_msgs::Point goal = get_goal_pose();
     
     Eigen::Vector3d f(curPose_.x, curPose_.y, curPose_.z);
     Eigen::Vector3d f_d(goal.x, goal.y, goal.z);
-    Eigen::Vector3d err;
-    Eigen::Vector3d t_dot;
-    Eigen::Vector3d j_dot;
+    Eigen::Vector2d err;
+    Eigen::Vector2d t_dot;      // velocity in task space
+    Eigen::Vector2d j_dot;      // velocity in joint space
 
-    err = f_d - f;
-    float lam = 0.035; // in m
-    float z = 0.1;
-    int kp = 1;
-    float l1 = 0.09;    // in m
-    float l2 = 0.065;   // in m
-    float th1 = 0;
-    float th2 = 0;
+    err(0) = f_d(0) - f(0);
+    err(1) = f_d(1) - f(1);
 
-    float jr1 = - l1*sin(th1*PI) - l2*sin((th1+th2)*PI);
-    float jr2 = - l2*sin((th1 + th2)*PI);
-    float jr3 = l1*cos(th1*PI) + l2*cos((th1 + th2)*PI);
-    float jr4 = l2*cos((th1 + th2)*PI);
+    float lam = 1.95; // in m
+    float z = 0.4;
+    int k = 32;
+    // float l1 = 0.09;    // in m
+    // float l2 = 0.065;   // in m
+    // float th1 = 45;
+    // float th2 = -45;
 
-    Eigen::MatrixXd Jv(2,2); // Image Jacobian
-    Jv << lam/z,0,
-         0,lam/z;
+    // float jr1 = - l1*sin(th1*PI) - l2*sin((th1+th2)*PI);
+    // float jr2 = - l2*sin((th1 + th2)*PI);
+    // float jr3 = l1*cos(th1*PI) + l2*cos((th1 + th2)*PI);
+    // float jr4 = l2*cos((th1 + th2)*PI);
+    
+    if(abs(err(1))>=0.001){
+    // Eigen::MatrixXd Jv(2,2); // Image Jacobian
+    // Jv << 1,0,
+    //       0,1;
 
     Eigen::MatrixXd Kp(2,2); //Gains matrix
-    Kp << kp, 0,
-          0, kp;
+    Kp << k, 0,
+          0, k;
 
-    Eigen::MatrixXd Jv_inv(2,2);
-    if(Jv.determinant() != 0)
-        Jv_inv = Jv.inverse();
+    // Eigen::MatrixXd Jv_inv(2,2);
+    // if(Jv.determinant() != 0)
+    //     Jv_inv = Jv.inverse();
 
-    Eigen::MatrixXd Jr(2,2); // Robot Jacobian 
-    Jr << jr1, jr2,
-          jr3, jr4;
+    // Eigen::MatrixXd Jr(2,2); // Robot Jacobian 
+    // Jr << jr1, jr2,
+    //       jr3, jr4;
+    // std::cout << "Jv: \n" << Jr_inv <<std::endl;
 
     Eigen::MatrixXd Jr_inv(2,2);
-    if(Jr.determinant() != 0)
-        Jr_inv = Jr.inverse();
+        Jr_inv << -15,  0,
+                   0, 15; //31. 15;
 
-    t_dot = (Kp * Jv_inv) * err;
-    
+    t_dot = -Kp * err;
+
     j_dot = Jr_inv * t_dot;
-    j_dot /= 10;        //normalization
-    
-    // Piecewise function
-    if(j_dot(1) > 1.0){
-        j_dot(1) = 1.0;
-    }
-    else if(j_dot(1) < -1.0){
-        j_dot(1) = -1.0;
-    }
-    else if(j_dot(1) > -0.45 && j_dot(1) < 0.45){
-        j_dot(1) = 0;
-    }
+    j_dot /= 25;        //normalization
+    printf("%f\n",j_dot(1));
 
-    if(j_dot(2) > 1.0){
-        j_dot(2) = 1.0;
+    // Piecewise function
+    // if(j_dot(0) >= 0.4){
+    //     j_dot(0) = 0.4;
+    // }
+    // else if(j_dot(0) <= -0.4){
+    //     j_dot(0) = -0.4;
+    // }
+    // else if(j_dot(0) < 0){
+    //     if(j_dot(0) > -0.2)
+    //         j_dot(0) = 0;
+    // }
+    // else if(j_dot(0) > 0){
+    //     if(j_dot(0) < 0.2)
+    //         j_dot(0) = 0;
+    // }
+
+    // if(j_dot(1) >= 0.4){
+    //     j_dot(1) = 0.4;
+    // }
+    // else if(j_dot(1) <= -0.4){
+    //     j_dot(1) = -0.4;
+    // }
+    // else if(j_dot(1) < 0){
+    //     if(j_dot(1) > -0.2)
+    //         j_dot(1) = 0;
+    // }
+    // else if(j_dot(1) > 0){
+    //     if(j_dot(1) < 0.2)
+    //         j_dot(1) = 0;
+    // }
+
+    if(j_dot(1)>0.3){
+        j_dot(1) = 0.3;
     }
-    else if(j_dot(2) < -1.0){
-        j_dot(2) = -1.0;
-    }
-    else if(j_dot(2) > -0.45 && j_dot(2) < 0.45){
-        j_dot(2) = 0;
+    if(j_dot(1)<-0.3){
+        j_dot(1) = -0.3;
     }
 
     std_msgs::Float64MultiArray vel_msg;    //Velocity Message for Pub
     vel_msg.data.resize(2);
     
-    vel_msg.data[0] = j_dot(1);
-    vel_msg.data[1] = j_dot(2);
+    vel_msg.data[0] = 0;//j_dot(0);
+    vel_msg.data[1] = j_dot(1);
 
     pub.publish(vel_msg);
 
     std_msgs::Float64MultiArray err_msg;
     err_msg.data.resize(3);
     
-    err_msg.data[0] = err(1);
-    err_msg.data[1] = err(2);
-    err_msg.data[2] = err(3);
+    err_msg.data[0] = err(0);
+    err_msg.data[1] = err(1);
+    err_msg.data[2] = 0;
 
     err_pub.publish(err_msg);
+    }
+    else{
+            std_msgs::Float64MultiArray vel_msg;    //Velocity Message for Pub
+            vel_msg.data.resize(2);
+    
+            vel_msg.data[0] = 0;//j_dot(0);
+            vel_msg.data[1] = 0;
 
+            pub.publish(vel_msg);
+            std_msgs::Float64MultiArray err_msg;
+            err_msg.data.resize(3);
+    
+            err_msg.data[0] = err(0);
+            err_msg.data[1] = err(1);
+            err_msg.data[2] = 0;
 
+    err_pub.publish(err_msg);
+    }
+}
 
+int main(int argc, char **argv){
+    
+    ros::init(argc, argv, "owi_controller");
+    // ros::NodeHandle n;
+    nh = new ros::NodeHandle;
 
+    
+    ros::Subscriber sub = nh->subscribe("object_position",1,get_cur_pose);
+    pub = nh->advertise<std_msgs::Float64MultiArray>("pwm",10);
+    err_pub = nh->advertise<std_msgs::Float64MultiArray>("error",10);
+    
     ros::spin();
     return 0;
 }
