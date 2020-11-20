4e35b8a52f9cbea423697b191d20d73445b4a407
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2020-11-19 22:06:28 -0500

    Angle computation

--- ros_ws/src/projects/soft_manipulation/marker_track/src/object_position_tracking_node.cpp
+++ ros_ws/src/projects/soft_manipulation/marker_track/src/object_position_tracking_node.cpp
@@ -8,61 +8,73 @@
 
 #define PI 57.2957795786
 
-float x1[3], x2[3], x3[3]; //x2 is the end effector, x3 is the elbow marker
+float base[3], ee[3], elbow[3], l2[4]; 
+
 float Orientation_Z;
 float Orientation_Reference_Z; 
 
-void Callback1(const geometry_msgs::Pose &msg1)
+void baseCallback(const geometry_msgs::PointStamped &msg1)
 {
-	x1[0]=(msg1.position.x); // - 0.0057;  // Adding an offset to the marker1
-	x1[1]=msg1.position.y;
-	x1[2]=msg1.position.z;
+	base[0]=msg1.point.x; // - 0.0057;  // Adding an offset to the marker1
+	base[1]=msg1.point.y;
+	base[2]=0;
 	for(int i= 0;i<3;i++)
 	{
 		printf("pose1: ");
-		printf("%f\n",x1[i]);
+		printf("%f\n",base[i]);
 	}
-	tf::Quaternion q(
-        msg1.orientation.x,
-        msg1.orientation.y,
-        msg1.orientation.z,
-        msg1.orientation.w);
-	tf::Matrix3x3 m(q);
-    	double roll, pitch, yaw;
-    	m.getRPY(roll, pitch, yaw);
-    	Orientation_Reference_Z=yaw;
+	// tf::Quaternion q(
+    //     msg1.orientation.x,
+    //     msg1.orientation.y,
+    //     msg1.orientation.z,
+    //     msg1.orientation.w);
+	// tf::Matrix3x3 m(q);
+    // 	double roll, pitch, yaw;
+    // 	m.getRPY(roll, pitch, yaw);
+    // 	Orientation_Reference_Z=yaw;
     	
 }
 
-void Callback2(const geometry_msgs::Pose &msg2)
+void eeCallback(const geometry_msgs::PointStamped &msg2)
 {
-	x2[0]=msg2.position.x;
-	x2[1]=msg2.position.y;
-	x2[2]=msg2.position.z;
+	ee[0]=msg2.point.x;
+	ee[1]=msg2.point.y;
+	ee[2]=0;
 	for(int i= 0;i<3;i++)
 	{
 		printf("pose2: ");
-		printf("%f\n", x2[i]);
+		printf("%f\n", ee[i]);
 	}
-	tf::Quaternion q(
-        msg2.orientation.x,
-        msg2.orientation.y,
-        msg2.orientation.z,
-        msg2.orientation.w);
-	tf::Matrix3x3 m(q);
-    	double roll, pitch, yaw;
-    	m.getRPY(roll, pitch, yaw);
-    	Orientation_Z=yaw;
+	// tf::Quaternion q(
+    //     msg2.orientation.x,
+    //     msg2.orientation.y,
+    //     msg2.orientation.z,
+    //     msg2.orientation.w);
+	// tf::Matrix3x3 m(q);
+    // 	double roll, pitch, yaw;
+    // 	m.getRPY(roll, pitch, yaw);
+    // 	Orientation_Z=yaw;
 }
 
-void Callback3(const geometry_msgs::Pose &msg3){
-	x3[0]=msg3.position.x;
-	x3[1]=msg3.position.y;
-	x3[2]=msg3.position.z;
+void elbowCallback(const geometry_msgs::PointStamped &msg3){
+	elbow[0]=msg3.point.x;
+	elbow[1]=msg3.point.y;
+	elbow[2]=0;
 	for(int i= 0;i<3;i++)
 	{
 		printf("pose3: ");
-		printf("%f\n", x3[i]);
+		printf("%f\n", elbow[i]);
+	}
+}
+
+void l2Callback(const geometry_msgs::PointStamped &msg4){
+	l2[0]=msg4.point.x;
+	l2[1]=msg4.point.y;
+	l2[2]=0;
+	for(int i= 0;i<3;i++)
+	{
+		printf("pose4: ");
+		printf("%f\n", l2[i]);
 	}
 }
 
@@ -74,73 +86,45 @@ int main(int argc, char **argv)
 
 	ros::NodeHandle n;
 
-	ros::Subscriber sub1 = n.subscribe("/aruco_simple/pose", 1000, Callback1);
-	ros::Subscriber sub2 = n.subscribe("/aruco_simple/pose2", 1000, Callback2);
-	ros::Subscriber sub3 = n.subscribe("/aruco_simple/pose3", 1000, Callback3);
+	ros::Subscriber sub1 = n.subscribe("/aruco_simple/pixel", 1000, baseCallback);
+	ros::Subscriber sub2 = n.subscribe("/aruco_simple/pixel2", 1000, eeCallback);
+	ros::Subscriber sub3 = n.subscribe("/aruco_simple/pixel3", 1000, elbowCallback);
+	ros::Subscriber sub4 = n.subscribe("/aruco_simple/pixel4", 1000, l2Callback);
 	
-	ros::Publisher pub = n.advertise<geometry_msgs::Point>("/object_position", 1000);
-	ros::Publisher pub1 = n.advertise<std_msgs::Float32>("/object_orientation", 1000);
+	// ros::Publisher pub = n.advertise<geometry_msgs::Point>("/object_position", 1000);
+	// ros::Publisher pub1 = n.advertise<std_msgs::Float32>("/object_orientation", 1000);
 	ros::Publisher theta_pub = n.advertise<geometry_msgs::Point>("/theta_vals", 1000);
-	ros::Publisher elbow_pub = n.advertise<geometry_msgs::Point>("/elbow_position", 1000);
+	// ros::Publisher elbow_pub = n.advertise<geometry_msgs::Point>("/elbow_position", 1000);
 	
 	ros::Rate r(20);
 	while(ros::ok())
 	{
-		for (int i=0; i<3; i++)
-		{
-			x[i] = x2[i]-x1[i];
-			elbow_marker[i] = x3[i] - x1[i];
-		}
-
-		geometry_msgs::Point msg;
-		msg.x = -x[0];
-		msg.y = x[1];
-		msg.z = x[2];
-		// std_msgs::Float32 Block_orientation;
-		// Block_orientation.data= Orientation_Z - Orientation_Reference_Z;
-
-		geometry_msgs::Point elbow_msg;
-		msg.x = - elbow_marker[0];
-		msg.y = elbow_marker[1];
-		msg.z = elbow_marker[2];
 
 		// Computing angles
 		float theta1 = 0;
 		float theta2 = 0;
 
-		if(x3[0]!= x1[0]){
-			theta1 = atan2((x3[1] - x1[1]),(x1[0] - x3[0])) * PI; //Since x is opposite dirn
+		if(elbow[0]!= base[0]){
+			theta1 = atan((elbow[1] - base[1])/(elbow[0] - base[0])) * PI; //Since x is opposite dirn
 		}
 		else
 		{
 			theta1 = 90;	
 		}
 
-		if(x2[0] != x1[0]){
-			theta2 = (atan2((x2[1] - x1[1]),(x1[0] - x2[0])) * PI) - theta1; //Since x is opp dirn
+		if(ee[0] != l2[0]){
+			theta2 = (atan((ee[1] - l2[1])/(ee[0] - l2[0])) * PI) - theta1; //Since x is opp dirn
 		}
 		else
 		{
-			theta2 = 90 - theta1;
+			theta2 = 90;
 		}
 		geometry_msgs::Point angle_msg;
 		angle_msg.x = theta1;
 		angle_msg.y = theta2;
 		angle_msg.z = 0.0;
 
-		//ROS_INFO("publishing the pose difference");
-
-		pub.publish(msg);
-		elbow_pub.publish(elbow_msg);
 		theta_pub.publish(angle_msg);
-		// pub1.publish(Block_orientation);
-
-		for(int i= 0;i<3;i++)
-		{
-			printf("***POSE***: ");
-			printf("%f\n", x[i]);
-			printf("%f\n",elbow_marker[i]);
-		}
 
 		ros::spinOnce();
 		r.sleep();
