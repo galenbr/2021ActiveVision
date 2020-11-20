ae2454ae64348dd6bd122107ecea8c277b99619a
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2020-11-20 14:56:57 -0500

    cleaned visual servo code

--- ros_ws/src/projects/soft_manipulation/owi_control/src/owi_controller.cpp
+++ ros_ws/src/projects/soft_manipulation/owi_control/src/owi_controller.cpp
@@ -6,6 +6,7 @@
 #include "std_msgs/Float64.h"
 #include "geometry_msgs/Vector3.h"
 #include "geometry_msgs/Point.h"
+#include "geometry_msgs/PointStamped.h"
 
 #define PI 3.14159265/180
 
@@ -27,11 +28,11 @@ geometry_msgs::Point get_goal_pose(){
     return goalPose;
 }
 
-void get_cur_pose(const geometry_msgs::Point &msg){
+void get_cur_pose(const geometry_msgs::PointStamped &msg){
 
-    curPose_.x = msg.x;
-    curPose_.y = msg.y;
-    curPose_.z = msg.z;   
+    curPose_.x = msg.point.x;
+    curPose_.y = msg.point.y;
+    curPose_.z = msg.point.z;   
 
     geometry_msgs::Point goal = get_goal_pose();
     
@@ -46,87 +47,48 @@ void get_cur_pose(const geometry_msgs::Point &msg){
 
     float lam = 1.95; // in m
     float z = 0.4;
-    int k = 320;
-    // float l1 = 0.09;    // in m
-    // float l2 = 0.065;   // in m
-    // float th1 = 45;
-    // float th2 = -45;
-
-    // float jr1 = - l1*sin(th1*PI) - l2*sin((th1+th2)*PI);
-    // float jr2 = - l2*sin((th1 + th2)*PI);
-    // float jr3 = l1*cos(th1*PI) + l2*cos((th1 + th2)*PI);
-    // float jr4 = l2*cos((th1 + th2)*PI);
+    int k = 1;
     
-    if(abs(err(1))>=0.0001){
-    // Eigen::MatrixXd Jv(2,2); // Image Jacobian
-    // Jv << 1,0,
-    //       0,1;
+    // Send robot joint velocities if error is greater than 1 px in either direction
 
-    Eigen::MatrixXd Kp(2,2); //Gains matrix
+    if(abs(err(1))>= 1 || abs(err(0)>= 1)){
+
+    Eigen::MatrixXd Kp(2,2); //Gains matrix, this incorporates the image Jacobian since it is constant for a 2D case
     Kp << k, 0,
           0, k;
 
-    // Eigen::MatrixXd Jv_inv(2,2);
-    // if(Jv.determinant() != 0)
-    //     Jv_inv = Jv.inverse();
-
-    // Eigen::MatrixXd Jr(2,2); // Robot Jacobian 
-    // Jr << jr1, jr2,
-    //       jr3, jr4;
-    // std::cout << "Jv: \n" << Jr_inv <<std::endl;
+    // Robot jacobian is updated using the update_jacobian()
+    // We invert this updated jacobian
+    std::cout << "Jr: \n" << Jr <<std::endl;
 
     Eigen::MatrixXd Jr_inv(2,2);
-        Jr_inv << -15,  0,
-                   0, 15; //31. 15;
+    Jr_inv = Jr.inverse();
 
     t_dot = -Kp * err;
 
     j_dot = Jr_inv * t_dot;
-    j_dot /= 25;        //normalization
+    j_dot /= 2400;        //normalization
     printf("%f\n",j_dot(1));
 
     // Piecewise function
-    // if(j_dot(0) >= 0.4){
-    //     j_dot(0) = 0.4;
-    // }
-    // else if(j_dot(0) <= -0.4){
-    //     j_dot(0) = -0.4;
-    // }
-    // else if(j_dot(0) < 0){
-    //     if(j_dot(0) > -0.2)
-    //         j_dot(0) = 0;
-    // }
-    // else if(j_dot(0) > 0){
-    //     if(j_dot(0) < 0.2)
-    //         j_dot(0) = 0;
-    // }
-
-    // if(j_dot(1) >= 0.4){
-    //     j_dot(1) = 0.4;
-    // }
-    // else if(j_dot(1) <= -0.4){
-    //     j_dot(1) = -0.4;
-    // }
-    // else if(j_dot(1) < 0){
-    //     if(j_dot(1) > -0.2)
-    //         j_dot(1) = 0;
-    // }
-    // else if(j_dot(1) > 0){
-    //     if(j_dot(1) < 0.2)
-    //         j_dot(1) = 0;
-    // }
-
-    if(j_dot(1)>0.3){
-        j_dot(1) = 0.3;
+    if(j_dot(0)>0.35){
+        j_dot(0) = 0.35;
     }
-    if(j_dot(1)<-0.3){
-        j_dot(1) = -0.3;
+    if(j_dot(0)<-0.35){
+        j_dot(0) = -0.35;
+    }
+
+    if(j_dot(1)>0.35){
+        j_dot(1) = 0.35;
+    }
+    if(j_dot(1)<-0.35){
+        j_dot(1) = -0.35;
     }
 
     std_msgs::Float64MultiArray vel_msg;    //Velocity Message for Pub
     vel_msg.data.resize(2);
     
-    vel_msg.data[0] = 0;//j_dot(0);
+    vel_msg.data[0] = j_dot(0);
     vel_msg.data[1] = j_dot(1);
 
     pub.publish(vel_msg);
@@ -164,8 +126,8 @@ void update_Jr(const geometry_msgs::Point &msg){
     float th1 = msg.x;
     float th2 = msg.y;
 
-    float l1 = 0.09;    // in m
-    float l2 = 0.065;   // in m
+    float l1 = 0.092;    // in m
+    float l2 = 0.09;   // in m
 
     float jr1 = - l1*sin(th1*PI) - l2*sin((th1+th2)*PI);
     float jr2 = - l2*sin((th1 + th2)*PI);
@@ -185,7 +147,7 @@ int main(int argc, char **argv){
     nh = new ros::NodeHandle;
 
     
-    ros::Subscriber sub = nh->subscribe("object_position",1,get_cur_pose);
+    ros::Subscriber sub = nh->subscribe("aruco_simple/pixel2",1,get_cur_pose);
     ros::Subscriber theta_sub = nh->subscribe("theta_vals",1,update_Jr);
     pub = nh->advertise<std_msgs::Float64MultiArray>("pwm",10);
     err_pub = nh->advertise<std_msgs::Float64MultiArray>("error",10);
