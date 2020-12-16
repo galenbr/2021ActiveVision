8aebe0ec3b86a627cfb7f5ae6a2dc2078b34688c
Sabhari <nsabhari@gmail.com>
2020-12-16 18:50:34 -0500

    Updating the graph code

--- ros_ws/src/projects/active_vision/scripts/csvSummarizer.py
+++ ros_ws/src/projects/active_vision/scripts/csvSummarizer.py
@@ -6,16 +6,10 @@ import numpy as np
 from prettytable import PrettyTable
 import matplotlib.pyplot as plt
 
-SAVE = True
-SAVE_PATH = '/home/diyogon/Documents/WPI/Berk_Lab/mer_lab/ros_ws/src/projects/active_vision/data/'
-
-def helpDisp(text):
-    print(text)
-    print('\n-----BFS Visualization Help-----\n')
-    print('Arguments : [CSV filename]')
-    print('CSV filename : CSV file name (./DataRecAV/Data.csv)')
-    print('\n-----End Help-----\n')
-    sys.exit()
+# mode = "GRAPH_VIEW"
+mode = "GRAPH_SAVE"
+# mode = "PRINT"
+# mode = "CSV"
 
 #Function to read the input data file
 def readInput(fileName):
@@ -68,38 +62,52 @@ def genSummary(fileName,nBins):
 def printSummary(summary):
     t = PrettyTable(['Bins(Yaw)', '#Data' , 'Min Steps', 'Avg Steps', 'Max Steps'])
     for each in summary:
-        fig, ax = plt.subplots()
-        degree = []
-        number = []
-        meanGrasp = []
-        titleStr = ''
         t.clear_rows()
+        t.title = each[0] + " - Roll : " + str(int(round(np.degrees(each[1]),0))) + " , Pitch : " + str(int(round(np.degrees(each[2]),0)))
         for eachYaw in each[3]:
-            t.title = each[0] + " - Roll : " + str(round(np.degrees(each[1]),0)) + " , Pitch : " + str(round(np.degrees(each[2]),0))
-            if(eachYaw[0] <= 180 and eachYaw[1] > 0):
+            if eachYaw[1] > 0:
                 t.add_row([eachYaw[0] ,eachYaw[1], eachYaw[2], eachYaw[3], eachYaw[4]])
-                degree.append(eachYaw[0])
-                number.append(eachYaw[1])
-                meanGrasp.append(eachYaw[3])
-                titleStr = "{}-roll {} pitch {}".format(each[0], str(round(np.degrees(each[1]),0)), str(round(np.degrees(each[2]),0)))
+            else:
+                t.add_row([eachYaw[0] , " ", " " , " " , " "])
         print(t)
-        color = 'tab:green'
+
+def graphSummary(path,summary):
+    for each in summary:
+        fig, ax = plt.subplots()
+        fig.set_size_inches(15, 4)
+        degree = []; number = []
+        minGrasp = []; meanGrasp = []; maxGrasp = []
+        titleStr = "{}-Roll_{}_Pitch_{}".format(each[0], str(int(round(np.degrees(each[1]),0))), str(int(round(np.degrees(each[2]),0))))
+        for eachYaw in each[3]:
+            degree.append(eachYaw[0])
+            number.append(eachYaw[1])
+            minGrasp.append(float(eachYaw[2]))
+            meanGrasp.append(float(eachYaw[3]))
+            maxGrasp.append(float(eachYaw[4]))
+
+        ax.set_xticks(degree)
+        ax.set_yticks(np.arange(max(maxGrasp)))
+        ax.grid(axis='y')
         ax.set_xlabel('yaw')
-        ax.set_ylabel('number of datapoints', color=color)
-        ax.bar(degree, number, width=8, color=color)
-        ax.tick_params(axis='y', labelcolor=color)
+        ax.set_ylabel('Steps to grasp')
+        ax.tick_params(axis='y', labelcolor="k")
+        ax.bar(degree, maxGrasp, width=8, color="tab:red", label='Max Steps')
+        ax.bar(degree, meanGrasp, width=8, color="tab:orange", label='Avg Steps')
+        ax.bar(degree, minGrasp, width=8, color="tab:green", label='Min Steps')
+        ax.legend()
         ax2 = ax.twinx()
         color = 'tab:blue'
-        ax2.set_ylabel('mean steps to grasp', color=color)
-        ax2.plot(degree, meanGrasp, color=color)
+        ax2.set_ylabel('# Datapoints', color=color)
+        ax2.plot(degree, number, color=color, marker = "o")
         ax2.tick_params(axis='y', labelcolor=color)
-        #fig.tight_layout()
         fig.suptitle(titleStr)
-        if(SAVE):
-            plt.savefig(SAVE_PATH+titleStr+'.png')
-        else:
+
+        if(mode == "GRAPH_SAVE"):
+            newPath = path[:path.rfind('/')+1] + titleStr + ".png"
+            plt.savefig(newPath)
+            print("Graph save to : "+newPath)
+        elif(mode == "GRAPH_VIEW"):
             plt.show()
-        print("---")
 
 def saveToCSV(path,summary):
     newPath = path[:-4] + "_summary.csv"
@@ -114,8 +122,15 @@ def saveToCSV(path,summary):
 if __name__ == "__main__":
     os.chdir(os.path.expanduser("~"))
 
-    path = '/home/diyogon/Documents/WPI/Berk_Lab/mer_lab/ros_ws/src/projects/active_vision/data/2020_12_15_230829_dataRec.csv'
+    # path = '/home/diyogon/Documents/WPI/Berk_Lab/mer_lab/ros_ws/src/projects/active_vision/data/2020_12_15_230829_dataRec.csv'
+    # path = './DataCollected/prismData/Prism_20x6x5_2000pts/Prism_20x6x5_2000pts.csv'
+    path = './DataCollected/prismData/Prism_10x8x4_2000pts/Prism_10x8x4_2000pts.csv'
     nBins = 360/10
 
     summary = genSummary(path,nBins)
-    printSummary(summary)
+    if(mode == "PRINT"):
+        printSummary(summary)
+    elif(mode == "GRAPH_VIEW" or mode == "GRAPH_SAVE"):
+        graphSummary(path,summary)
+    elif(mode == "CSV"):
+        saveToCSV(path,summary)
