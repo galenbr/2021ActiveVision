46bea11232b74666aa071cd25587608bcd138ed4
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2021-01-12 14:19:45 -0500

    Added ros shutdwown topic

--- ros_ws/src/projects/soft_manipulation/owi_control/src/const_vel.cpp
+++ ros_ws/src/projects/soft_manipulation/owi_control/src/const_vel.cpp
@@ -11,12 +11,15 @@ int main(int argc, char **argv){
 
     ros::Publisher sin_pub = n.advertise<std_msgs::Float64MultiArray>("pwm",1);
     ros::Publisher action_pub = n.advertise<std_msgs::Float64>("action",1);
+    ros::Publisher shutdown_pub = n.advertise<std_msgs::Float64>("shutdown",1);
+
     float count = 0;
     float vel;
     std_msgs::Float64MultiArray vel_msg;
     vel_msg.data.resize(2);
 
     std_msgs::Float64 action_msg;
+    std_msgs::Float64 shutdown_msg;
 
     ros::Rate r{30};
     while(ros::ok()){
@@ -27,8 +30,12 @@ int main(int argc, char **argv){
             vel_msg.data[1] = 0;
             sin_pub.publish(vel_msg);
             count += 1;
+            
             action_msg.data = 1;
             action_pub.publish(action_msg);
+
+            shutdown_msg.data = 0.0;
+            shutdown_pub.publish(shutdown_msg);
         }
         else{
             vel_msg.data[0] = 0;
@@ -38,7 +45,9 @@ int main(int argc, char **argv){
             ROS_INFO("DONE");
             count += 1;
             if(count >200){
-                // Add shutdown msg for all nodes
+                shutdown_msg.data = 1.0;
+                shutdown_pub.publish(shutdown_msg);
+
                 ros::shutdown();
             }
         }
