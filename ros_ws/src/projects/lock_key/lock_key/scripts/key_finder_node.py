7f1a28b4837425e53563abff2f42f14005990003
mech0ctopus <mech0ctopus@users.noreply.github.com>
2021-01-12 19:48:18 -0500

    key_finder_node is functional

--- ros_ws/src/projects/lock_key/lock_key/scripts/key_finder_node.py
+++ ros_ws/src/projects/lock_key/lock_key/scripts/key_finder_node.py
@@ -16,6 +16,7 @@ class FinderPub:
     '''Finds lock and key from RGBD data.'''
     def __init__(self, key_search_box, key_min_hsv, key_max_hsv, 
                  lock_search_box, lock_min_hsv, lock_max_hsv):
+        self.header=None
         self.key_search_box=key_search_box
         self.key_min_hsv=key_min_hsv
         self.key_max_hsv=key_max_hsv
@@ -26,89 +27,64 @@ class FinderPub:
         self.lock_max_hsv=lock_max_hsv
         self.lock_center=None
         self.lock_center_depth=None
+        self.rgb_camera=image_geometry.PinholeCameraModel()
+        self.rgb_camera_info_received=False
         self.rgb_image=None
         self.rgb_received=False
         self.depth_array=None
         self.depth_received=False
-        #ROS
+        #ROS Subscribers and Publishers
         self.bridge=CvBridge()
         self.rgb_sub=rospy.Subscriber('/camera/color/image_raw',
                                       Image,self.rgb_callback)
-        self.depth_sub=rospy.Subscriber('/depth_registered/image_rect', #/camera/depth/image_rect_raw
+        self.depth_sub=rospy.Subscriber('/camera/depth/image_rect_raw', #/depth_registered/image_rect
                                         Image,self.depth_callback)
         self.rgb_info=rospy.Subscriber('/camera/color/camera_info',
-                                      Image,self.rgb_callback)
+                                       CameraInfo,self.rgb_info_callback)
         self.vis_pub = rospy.Publisher('finder_image', Image, queue_size=10)
         self.key_pub = rospy.Publisher('key_point', PointStamped, 
                                        queue_size=10)
         self.lock_pub = rospy.Publisher('lock_point', PointStamped, 
                                         queue_size=10)
-    
-    def calculate_positions(self):
+        rospy.loginfo('Running lock and key finder node.')
+
+    def calculate_positions(self,img):
         '''Calculates centers of lock and key.'''
         # Get lock and key centers (in RGB image) in terms of pixels
         # RGB and Depth are registered so pixel locations are the same
-        vis_img=self.get_centers()
+        vis_img=self.get_centers(img)
         # Get corresponding depth values (in meters)       
-        self.lock_center_depth=self.depth_img[self.lock_center[0],
-        									  self.lock_center[1]]
-        self.key_center_depth=self.depth_img[self.key_center[0],
-        									 self.key_center[1]]
-        # Get XYZ displacement in terms of meters (in camera_link)
-		X = Z / fx * (u - cx)
-		Y = Z / fy * (v - cy)
+        self.lock_center_depth=self.depth_array[self.lock_center[1],
+        									    self.lock_center[0]]
+        self.key_center_depth=self.depth_array[self.key_center[1],
+        									   self.key_center[0]]
+        # Get normalized XYZ displacement (in camera_link)
+        lock_disp=self.rgb_camera.projectPixelTo3dRay(self.lock_center)
+        key_disp=self.rgb_camera.projectPixelTo3dRay(self.key_center)
+        #Get scale from depth
+        lock_scale=self.lock_center_depth/lock_disp[2]/1000.0
+        key_scale=self.key_center_depth/key_disp[2]/1000.0
+        #Convert to meters
+        lock_disp=lock_scale*np.array(lock_disp)
+        key_disp=key_scale*np.array(key_disp)
         # Publish center and visualization image
         self.publish_points(lock_disp,key_disp)
-        publish_vis(vis_img)
+        self.publish_vis(vis_img)
         
     def depth_callback(self,raw_depth_img):
     	'''Converts depth image to np.array with actual depth.'''
-    	try:
-		    #Convert to cv2 format
-		    depth_img=self.bridge.imgmsg_to_cv2(raw_depth_img, 
-		    									desired_encoding="passthrough")
-	        #Convert the depth image to a Numpy array (with actual depth values)
-	        self.depth_array = np.array(depth_img, dtype=np.float32)
-	    	self.depth_received=True
-	    except CvBridgeError, e:
-	        print e
+        try:
+            #Convert to cv2 format
+            depth_img=self.bridge.imgmsg_to_cv2(raw_depth_img, 
+            									desired_encoding="passthrough")
+            #Convert the depth image to a Numpy array (with actual depth values)
+            self.depth_array = np.array(depth_img, dtype=np.float32)
+            self.depth_received=True
+        except CvBridgeError, e:
+            print e
 
-    def publish_points(self,lock_disp,key_disp):
-        '''Publish lock and key positions in terms of camera_link.'''
-        #Build header
-        h=Header()
-        h.stamp=self.rgb_img.header.stamp
-        h.frame_id='camera_link'
-        #Build lock msg
-        lock_msg=PointStamped()
-        lock_msg.header=h
-        lock_msg.Point=lock_disp
-        #Build key msg
-        key_msg=PointStamped()
-        key_msg.header=h
-        key_msg.Point=key_disp
-        #Publish messages
-        self.lock_pub.publish(lock_msg)
-        self.key_pub.publish(key_msg)
-
-    def publish_vis(self,vis_img):
-        '''Define and publish visualization image'''
-        h=Header()
-        h.stamp=self.rgb_img.header.stamp
-        h.frame_id='camera_link'
-        vis_img=self.bridge.cv2_to_imgmsg(vis_img,"bgr8")
-        vis_img.header=h
-        self.vis_pub.publish(vis_img)        
-        
-    def rgb_callback(self,raw_rgb_img):   
-        '''Convert to cv2 format and store image.'''
-        self.rgb_img=self.bridge.imgmsg_to_cv2(raw_rgb_img,"bgr8")
-        self.rgb_received=True
-        self.calculate_positions()
-        
-    def get_centers(self):
+    def get_centers(self,img):
         '''Compute object centers and adds visuals to image.'''
-        img=self.current_rgb_image
         #Update img for key
         img,self.key_center=image_utils.color_change(img,
                                                      self.key_search_box,
@@ -138,6 +114,55 @@ class FinderPub:
                                 color=[0,200,200])
         
         return img
+
+    def publish_points(self,lock_disp,key_disp):
+        '''Publish lock and key positions in terms of camera_link.'''
+        #Build header
+        h=Header()
+        h.stamp=self.header.stamp
+        h.frame_id='camera_link'
+        #Build lock msg
+        lock_msg=PointStamped()
+        lock_msg.header=h
+        lock_msg.point.x=lock_disp[0]
+        lock_msg.point.y=lock_disp[1]
+        lock_msg.point.z=lock_disp[2]
+        #Build key msg
+        key_msg=PointStamped()
+        key_msg.header=h
+        key_msg.point.x=key_disp[0]
+        key_msg.point.y=key_disp[1]
+        key_msg.point.z=key_disp[2]
+        #Publish messages
+        self.lock_pub.publish(lock_msg)
+        self.key_pub.publish(key_msg)
+
+    def publish_vis(self,vis_img):
+        '''Define and publish visualization image'''
+        h=Header()
+        h.stamp=self.header.stamp
+        h.frame_id='camera_link'
+        vis_img=self.bridge.cv2_to_imgmsg(vis_img,"bgr8")
+        vis_img.header=h
+        self.vis_pub.publish(vis_img)        
+
+    def rgb_callback(self,raw_rgb_img):   
+        '''Convert to cv2 format and store image.'''
+        self.header=raw_rgb_img.header
+        self.rgb_img=self.bridge.imgmsg_to_cv2(raw_rgb_img,desired_encoding="bgr8")
+        if self.rgb_img is not None:
+            self.rgb_received=True
+
+        if (self.rgb_received and self.depth_received and self.rgb_camera_info_received) and \
+           (self.rgb_img is not None) and (self.depth_array is not None):
+            self.calculate_positions(self.rgb_img)
+
+    def rgb_info_callback(self,rgb_cam_info):   
+        '''Store camera info. in Pinhole Model.'''
+        if not self.rgb_camera_info_received:
+            self.rgb_camera.fromCameraInfo(rgb_cam_info)
+            self.rgb_camera_info_received=True
+            rospy.loginfo('RGB Camera Info. Received')
         
 def main():
     #Define input parameters
