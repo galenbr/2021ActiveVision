6a0a09a0a2f5527664746578332d825c00dc4d13
Abhinav Gandhi <abhinavgandhi09@gmail.com>
2021-01-14 16:24:48 -0500

    Added computations for functions

--- Matlab/soft_manipulation/owi_angular_velocity/compute_angle.m
+++ Matlab/soft_manipulation/owi_angular_velocity/compute_angle.m
@@ -1,8 +1,16 @@
-function [theta] = compute_angle(base_marker,ee_marker)
+function [theta] = compute_angle(base,ee)
 theta = [0];
 
-for i=1:size(base_marker)
-    theta[i] = atan()
+for i=1:size(base)
+    if(ee(i,2) ~= base(i,2))
+        if(ee(i,2) < base(i,2))
+            theta(i) = (atan((ee(i,3) - base(i,3))/(ee(i,2) - base(i,2))))*57.2958;
+        else
+            theta(i) = 180 + ((atan((ee(i,3) - base(i,3))/(ee(i,2) - base(i,2))))*57.2958);
+        end
+    else
+        theta(i) = 90;
+    end
 end
 
 
--- Matlab/soft_manipulation/owi_angular_velocity/compute_angularvel2.m
+++ Matlab/soft_manipulation/owi_angular_velocity/compute_angularvel2.m
@@ -1,5 +1,15 @@
-function [] = compute_angularvel2()
-
+function [ang_vel] = compute_angularvel2(theta, flag)
+time = 1/30;
+ang_vel(1) = 0;
+acc = 0;
+for i = 25:length(theta)
+    if(theta(i)- theta(i-1) ~= 0)
+        ang_vel(i) = (theta(i) - theta(i-1))/time;
+        acc = (ang_vel(i) - ang_vel(i-1))/time;
+    else
+        ang_vel(i) = ang_vel(i-1) + acc * time;
+    end
+end
 
 
 end
