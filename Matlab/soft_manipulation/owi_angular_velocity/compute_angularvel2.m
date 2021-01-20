63ade619fa24fe1b4d315bedd7ee858c7fa109d4
Abhinav Gandhi <abhinavgandhi09@gmail.com>
2021-01-19 22:59:34 -0500

    added theta computation for missing frames

--- Matlab/soft_manipulation/owi_angular_velocity/compute_angularvel2.m
+++ Matlab/soft_manipulation/owi_angular_velocity/compute_angularvel2.m
@@ -1,13 +1,14 @@
-function [ang_vel] = compute_angularvel2(theta, flag)
+function [ang_vel2] = compute_angularvel2(theta, flag)
 time = 1/30;
-ang_vel(1) = 0;
+ang_vel2(1) = 0;
 acc = 0;
 for i = 25:length(theta)
-    if(theta(i)- theta(i-1) ~= 0)
-        ang_vel(i) = (theta(i) - theta(i-1))/time;
-        acc = (ang_vel(i) - ang_vel(i-1))/time;
+    if(flag(i,2) == 1)
+        ang_vel2(i) = (theta(i) - theta(i-1))/time;
+        acc = (ang_vel2(i) - ang_vel2(i-1))/time;
     else
-        ang_vel(i) = ang_vel(i-1) + acc * time;
+        ang_vel2(i) = ang_vel2(i-1) + (acc * time);
+        theta(i) = theta(i-1) + (ang_vel2(i)*time);
     end
 end
 
