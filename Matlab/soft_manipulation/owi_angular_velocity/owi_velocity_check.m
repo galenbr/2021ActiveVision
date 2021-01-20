620e28f27c2a7f5de42a9052bec20db0ce94d212
Abhinav Gandhi <abhinavgandhi09@gmail.com>
2021-01-19 22:58:40 -0500

    Added moving average, cleaned code

--- Matlab/soft_manipulation/owi_angular_velocity/owi_velocity_check.m
+++ Matlab/soft_manipulation/owi_angular_velocity/owi_velocity_check.m
@@ -1,8 +1,9 @@
+clear;
 %% Reading data from csv
-base_marker = csvread('data\exp1\base_px.csv',2,0);
-ee_marker = csvread('data/exp1/px3.csv',2,0);
-flag = csvread('data/exp1/flag.csv',2,0);
-input_vel = csvread('data/exp1/input_vel.csv',2,0);
+base_marker = csvread('data\exp3\base_px.csv',2,0);
+ee_marker = csvread('data/exp3/px3.csv',2,0);
+flag = csvread('data/exp3/flag.csv',2,0);
+input_vel = csvread('data/exp3/input_vel.csv',2,0);
 
 %% Calculating joint angle
 theta = compute_angle(base_marker,ee_marker);
@@ -12,27 +13,38 @@ theta = compute_angle(base_marker,ee_marker);
 ang_vel1 = compute_angularvel1(theta,flag);
 % Case 2: assuming prev acceleration for dropped frames
 ang_vel2 = compute_angularvel2(theta,flag);
+% Case 3: moving average for droped frames
+ang_vel3 = compute_angularvel3(theta, flag);
 
 %% Plot
 % Joint angle vs frames
 figure('NumberTitle','off','Name','OWI Open Loop Performance')
-%title('OWI Open Loop Performance')
-subplot(4,1,1)
+
+subplot(6,1,1)
 plot(theta)
 title('Joint Angle')
-% Input pwm, ang_vel1 vs frames
-subplot(4,1,3)
+
+% using prev ang_vel
+subplot(6,1,3)
 plot(ang_vel1)
 title('Measured Angular Velocity')
-subplot(4,1,2)
+
+% input vel
+subplot(6,1,2)
 plot(input_vel(:,2))
 title('Input PWM')
-% Input pwm, ang_vel2 vs frames
-subplot(4,1,4)
+
+% with acc
+subplot(6,1,4)
 plot(ang_vel2)
 title('Measured Angular Velocity w/ Acc')
-% End effector position
-figure('NumberTitle','off','Name','End-effector Position')
-plot()
-% Time plot of end-effector positions and estimated positions for dropped
-% frames
+
+% Moving average
+subplot(6,1,5)
+plot(ang_vel3)
+title('Moving average')
+
+% Flag
+subplot(6,1,6)
+plot(flag(:,2))
+title('New frame check')
