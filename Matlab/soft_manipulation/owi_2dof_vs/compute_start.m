3480fd4fd7e0a3da26235c42b5ea61afa921928b
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2021-02-16 13:16:27 -0500

    fixed typo

--- Matlab/soft_manipulation/owi_2dof_vs/compute_start.m
+++ Matlab/soft_manipulation/owi_2dof_vs/compute_start.m
@@ -3,7 +3,7 @@ flag = 0;
 posn = 0;
 start = [0,0];
     for i = 1:length(input_vel)
-        if(input_vel(i,2) ==0 && input_vel(1,3) ==0)
+        if(input_vel(i,2) ==0 && input_vel(i,3) ==0)
             continue;
         else
             start = [ee(i,2),ee(i,3)];
