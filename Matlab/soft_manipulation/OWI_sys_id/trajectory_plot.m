94b4c0972c8f84d4d6f81f7d547cb2c7cb5370ee
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2021-02-16 14:05:50 -0500

    added cmd to save plots

--- Matlab/soft_manipulation/OWI_sys_id/trajectory_plot.m
+++ Matlab/soft_manipulation/OWI_sys_id/trajectory_plot.m
@@ -13,4 +13,7 @@ plot(start(1),start(2),'g+', 'MarkerSize', 15, 'LineWidth', 3)
 plot(ee(:,2),ee(:,3),'b.', 'MarkerSize', 7, 'LineWidth', 2)
 % line([start(1),goal(1)],[start(2),goal(2)], 'Color', 'green', 'LineWidth', 3)
 plot(goal(1),goal(2),'r+', 'MarkerSize', 15, 'LineWidth', 3)
+str_loc = "~/owi_data/sys_id_data/exp"+exp_no+"/trajectory.png";
+saveas(gcf, str_loc)
+
 end
