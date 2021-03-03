a141f038cfac3195b38605d64a46605d61490ae1
abhinavgandhi09 <abhinavgandhi09@gmail.com>
2021-03-03 17:50:51 -0500

    removed clustering, added pass through filter

--- ros_ws/src/projects/soft_manipulation/rrbot_sim/rrbot_pcl/src/process_cloud.cpp
+++ ros_ws/src/projects/soft_manipulation/rrbot_sim/rrbot_pcl/src/process_cloud.cpp
@@ -5,14 +5,7 @@
 #include "pcl/point_types.h"
 #include "pcl_conversions/pcl_conversions.h"
 #include <pcl/filters/voxel_grid.h>
-// #include <pcl/ModelCoefficients.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/kdtree/kdtree.h>
-// #include <pcl/sample_consensus/method_types.h>
-// #include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/passthrough.h>
 
 bool processCloud(rrbot_pcl::processCloud::Request &req, rrbot_pcl::processCloud::Response &res){
     
@@ -24,86 +17,28 @@ bool processCloud(rrbot_pcl::processCloud::Request &req, rrbot_pcl::processCloud
     pcl::fromPCLPointCloud2(*pcl_pc2, *cloud);
 
     // Downsampling
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_downsampled(new pcl::PointCloud<pcl::PointXYZRGB>);
     pcl::VoxelGrid<pcl::PointXYZRGB> sor;
     sor.setInputCloud (cloud);
     sor.setLeafSize(0.002f, 0.002f, 0.002f);
-    sor.filter(*cloud_filtered);
+    sor.filter(*cloud_downsampled);
 
-    // Segmentation
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZRGB>);
-    pcl::SACSegmentation<pcl::PointXYZRGB> seg;
-    pcl::PointIndices::Ptr inliers(new pcl::PointIndices); 
-    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZRGB> ());
-    seg.setOptimizeCoefficients(true);
-    seg.setModelType(pcl::SACMODEL_PLANE);
-    seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setMaxIterations(100);
-    seg.setDistanceThreshold(0.001);
+    // Filtering
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PassThrough<pcl::PointXYZRGB> pass;
+    pass.setInputCloud (cloud_downsampled);
+    pass.setFilterFieldName ("z");
+    pass.setFilterLimits (0.0, 3.1);
+    //pass.setFilterLimitsNegative (true);
+    pass.filter (*cloud_filtered);
 
-    int i=0, nr_points = (int) cloud_filtered->points.size ();
-    while (cloud_filtered->points.size () > 0.3 * nr_points){
-        // Segment the largest planar component from the remaining cloud
-        seg.setInputCloud (cloud_filtered);
-        seg.segment (*inliers, *coefficients);
-        if (inliers->indices.size () == 0){
-            std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
-            break;
-        }
-
-        // Extract the planar inliers from the input cloud
-        pcl::ExtractIndices<pcl::PointXYZRGB> extract;
-        extract.setInputCloud (cloud_filtered);
-        extract.setIndices (inliers);
-        extract.setNegative (false);
-
-        // Get the points associated with the planar surface
-        extract.filter (*cloud_plane);
-        std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size () << " data points." << std::endl;
-
-        // Remove the planar inliers, extract the rest
-        extract.setNegative (true);
-        extract.filter (*cloud_f);
-        *cloud_filtered = *cloud_f;
-    }
-
-    // Creating the KdTree object for the search method of the extraction
-    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
-    tree->setInputCloud (cloud_filtered);
-
-    std::vector<pcl::PointIndices> cluster_indices;
-    pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
-    ec.setClusterTolerance (0.02); // 2cm
-    ec.setMinClusterSize (100);
-    ec.setMaxClusterSize (10000);
-    ec.setSearchMethod (tree);
-    ec.setInputCloud (cloud_filtered);
-    ec.extract (cluster_indices);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZRGB>);
-    int j = 0;
-    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it){
-        
-        for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
-            cloud_cluster->points.push_back (cloud_filtered->points[*pit]); //*
-        cloud_cluster->width = cloud_cluster->points.size ();
-        cloud_cluster->height = 1;
-        cloud_cluster->is_dense = true;
-
-        // std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
-        // std::stringstream ss;
-        // ss << "cloud_cluster_" << j << ".pcd";
-        //writer.write<pcl::PointXYZRGB> (ss.str (), *cloud_cluster, false); //*
-        // j++;
-    }
-
-    // Convert Cloud Cluster to PCL
+    // Convert Cloud Cluster to PCL for debugging filtered cloud
     sensor_msgs::PointCloud2 ret;
     pcl::PCLPointCloud2 temppc2;
     pcl::toPCLPointCloud2(*cloud_filtered, temppc2);
     pcl_conversions::fromPCL(temppc2, ret);
-
     res.pc2 = ret;
+
 
     return true;
 }
